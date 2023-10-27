210e1255d1d5cba3058a6ac2568025e00a3a343e
L-jy16 <144635774+L-jy16@users.noreply.github.com>
2023-10-28 00:13:43 +0900

    2023.

--- webd/JUST 쇼핑몰/javascript/script.js
+++ webd/JUST 쇼핑몰/javascript/script.js
@@ -24,4 +24,24 @@ $(function(){
         $(".nav > ul > li > ul").stop().slideUp();
     });
 
+    
+    // 탭 메뉴
+    const tabBtn = $(".info-menu > a");
+    const tabCont = $(".info-cont > div");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.click(function(){
+        const index = $(this).index();
+
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+
+    // 팝업
+    $(".popup-btn").click(function(){
+        $(".popup-view").show()
+    });
+    $(".popup-close").click(function(){
+        $(".popup-view").hide()
+    });
 });
